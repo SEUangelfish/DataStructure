bcb53ed4ff22b81801ce82b41f19624fca7f56f5
angelfish <90554723+SEUangelfish@users.noreply.github.com>
2024-01-01 16:14:21 +0800

    Trie基本功能完成

--- Trie.h
+++ Trie.h
@@ -40,26 +40,10 @@ namespace dsl {
 		using _ElemAlloc = _Alloc<TrieNode<range>>;
 		using _Node = _Elemtype;
 
-	public:
-		Trie() = default;
-		Trie(_Map _map) :map(_map) {}
-
-		~Trie() {
-			this->Clear();
-			this->alloc.Free(this->root, 1);
-		}
-
-		// remove all the words from the trie
-		void Clear() {
-			_Node* u = this->root, * head = nullptr;
-			for (size_t i = 0; i < range; ++i) {
-				if ((*u)[i]) {
-					(*u)[i]->Words() = (size_t)head;
-					head = (*u)[i];
-					(*u)[i] = nullptr;
-				}
-			}
-			u->Words() = u->Prefix() = 0;
+	protected:
+		void Clear(_Node* rt) {
+			_Node* u, * head = rt;
+			head->Words() = NULL;
 			while (head) {
 				u = head;
 				head = (_Node*)head->Words();
@@ -74,7 +58,28 @@ namespace dsl {
 			}
 		}
 
-		// insert a word into the trie including null string 
+	public:
+		Trie() = default;
+		Trie(_Map _map) :map(_map) {}
+
+		~Trie() {
+			this->Clear();
+			this->alloc.Free(this->root, 1);
+		}
+
+		// remove all words from trie including null string 
+		void Clear() {
+			_Node* u = this->root;
+			u->Words() = u->Prefix() = 0;
+			for (size_t i = 0; i < range; ++i) {
+				if ((*u)[i]) {
+					this->Clear((*u)[i]);
+					(*u)[i] = nullptr;
+				}
+			}
+		}
+
+		// insert a word into the trie including null string (more accurately, the length equals 0)
 		void Insert(const _Ty* word, size_t length) {
 			_Node* u = this->root;
 			size_t idx;
@@ -92,6 +97,7 @@ namespace dsl {
 			++u->Words();
 		}
 
+		// return true if it contains the word including null string 
 		bool Contains(const _Ty* word, size_t length) {
 			if (!length) return this->root->Words();
 			_Node* u = this->root;
@@ -101,11 +107,12 @@ namespace dsl {
 				if ((*u)[idx] == nullptr) return false;
 				u = (*u)[idx];
 			}
-			return true;
+			return u->Words();
 		}
 
-		// remove the word from trie
+		// remove the word from trie including null string 
 		// ensure: true if it must contains the word
+		// ps: if there are the same words in trie, this call can only remove one
 		void Remove(const _Ty* word, size_t length, bool ensure) {
 			if (!ensure && !this->Contains(word, length)) return;
 			--this->root->Prefix();
@@ -133,13 +140,30 @@ namespace dsl {
 		}
 
 		// remove all words with prefix of parameter 1
-		//void Remove(const _Ty* prefix, size_t length) {
-		//	_Node* u = this->root;
-		//	size_t idx;
-		//	for (size_t i = 0; i < length; ++i) {
-
-		//	}
-		//}
+		// ensure: true if it must contains word with prefix of parameter 1
+		void RemoveAll(const _Ty* prefix, size_t length) {
+			if (!length) {
+				this->Clear();
+				return;
+			}
+			size_t cnt = this->CountWordsWithPrefix(prefix, length);
+			if (!cnt) return;
+			this->root->Prefix() -= cnt;
+			_Node* cur = this->root, * pre = nullptr;
+			size_t i = 0, idx;
+			for (; i < length; ++i) {
+				pre = cur;
+				idx = this->map(prefix[i]);
+				cur = (*cur)[idx];
+				if (!(cur->Prefix() -= cnt)) {
+					(*pre)[idx] = nullptr;
+					this->Clear(cur);
+					return;
+				}
+			}
+			(*pre)[idx] = nullptr;
+			this->Clear(cur);
+		}
 
 		// return number of words
 		size_t Size() {
@@ -156,8 +180,5 @@ namespace dsl {
 		_ElemAlloc alloc;
 		_Map map = [](const _Ty& x)->size_t { return x % 26; };
 		_Node* root = this->alloc.New0(1);
-
-		//bool startsWithPrefix(string prefix);
-		//void traverse();
 	};
 }
